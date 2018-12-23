5ee2131311a57648639641312e3b2b7b21a398a5
Ahmed Akhtar <32972045+ahmedakhtar11@users.noreply.github.com>
2018-12-22 20:11:34 -0600

    Update Virtualassistant.java

--- src/Virtualassistant.java
+++ src/Virtualassistant.java
@@ -4,78 +4,125 @@
 // Example Inputs: hello, goodbye, date
 
 
+
+public class Calculator {
+	
+	public double calculate(int first, int second, String operator) {
+		if (operator.equals("+")) {
+			return 1.0 * first + second;
+		} else if (operator.equals("-")) {
+			return 1.0 * first - second;
+		} else if (operator.equals("*")) {
+			return 1.0 * first * second;
+		} else if (operator.equals("/")) {
+			return 1.0 * first / second;
+		}
+	return 0.0;
+	}
+}
+
 import java.util.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import java.time.Instant;
-import java.time.ZonedDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+public class Virtualassistant {
 
-    public class Virtualassistant {
+// Creating Method (Called Later)
+static void Greeting() {
+		System.out.println("Hello I am Oracle, your Virtual Assistant.");
+	 }
+	 
+static void Goodbye() {
+	System.out.println("See you Later. We shall be reunited one day.");
+}
 
-        // Creating Method (Called Later)
-        static void Greeting() {
-            System.out.println("Hello I am Oracle, your Virtual Assistant.");
-        }
-
-        static void Goodbye() {
-            System.out.println("See you Later. We shall be reunited one day.");
-        }
-
-        // Getting Date and Time
-        static void get_Date_and_Time()
-        {
-            Calendar calender=Calendar.getInstance();
-            Date date=calender.getTime();
-            System.out.println("Current Date and Time is:");
-            System.out.println(date.toLocaleString());
-        }
+// Getting Date and Time
+static void date_and_time()
+	{
+Calendar calender=Calendar.getInstance();
+Date date=calender.getTime();
+System.out.println("Current Date and Time is:");
+		System.out.println(date.toLocaleString());
+	}
 
 
-        public static void main(String[] args)
+public static void main(String[] args) 
 
-        {
+{
 
 // Setting up Input
-            Scanner myVar = new Scanner(System.in);
+Scanner myVar = new Scanner(System.in);
 
 
 // Putting Input into String
 // Alternative: myVar.nextLine();
-            String entry = myVar.next();
+String entry = myVar.next();
+ 
+ 
+//Basic Regex Matching
+if (entry.matches("(hi|hello|yo|wassup|whats up|good morning|Hello|Hi|good afternoon|how are you|howdy)\\b.*")) {
+System.out.println("whats up doc?");
 
-            switch(entry) {
-
-                case "hello":
-                    Greeting();
-                    break;
-
-                case "goodbye":
-                    Goodbye();
-                    break;
-
-                default:
-                    System.out.println("I'm very sorry there. I do not seem to Understand.");
-
-            }
-
-        }
-    }
-
-/*
-//If Else Statement Usage
-// General Answers
-if (entry.equals("hello")) {
-	Greeting();
 }
-else if (entry.equals("goodbye")) {
-	Goodbye();
-}
- else {
-	 System.out.println("I'm very sorry there. I do not seem to Understand.");
- }
-*/
 
+else if (entry.matches("(peace|bye|Bye|adios|farewell|good night|goodnight)\\b.*")) {
+System.out.println("Goodbye then");
+	
+}
+
+else if (entry.matches("(?s).*(\\b[0-9]*)(\\s)?([-|+|*|/])(\\s)?([0-9]*).*")) {
+System.out.println("Calculator Initialized!");
+Calculator calculator = new Calculator();
+			int firstNumber = 0;
+			int secondNumber = 0;
+			String firstString = "";
+			String secondString = "";
+			String operator = "";
+			int overallIndex = 0;
+			
+for (int index = 0; index < entry.length(); index++) {
+				String chaString = Character.toString(entry.charAt(index));
+			
+			
+if (chaString.matches("[-|+|*|/]")) {
+					operator += chaString;
+					overallIndex = index;
+					break;
+				}
+				if (chaString.matches("[0-9]")) {
+					firstString += chaString;
+				}
+			}
+			
+			for (int index = overallIndex + 1; index < entry.length(); index++) {
+				String chaString = Character.toString(entry.charAt(index));
+				if (chaString.matches("[0-9]")) {
+					secondString += chaString;
+				}
+			}
+			
+			
+			if (!(firstString.isEmpty())) {
+				firstNumber = Integer.parseInt(firstString);
+			}
+			if (!(secondString.isEmpty())) {
+				secondNumber = Integer.parseInt(secondString);
+			}
+			
+			
+			double calculation = calculator.calculate(firstNumber, secondNumber, operator);
+			if ((calculation % 1) == 0) {
+				int intCalculation = (int)calculation;
+				System.out.println(firstNumber + " " + operator + " " + secondNumber + " = " + intCalculation);
+
+} 
+}
+
+//Default
+else {
+System.out.println("I'm sorry I do not understand");
+
+       }
+   }
+}
